3fef9e9a156f7fbaebd25867e6e534c9091dc618
Giovanni Carrara <carrara.g@gmail.com>
2018-04-24 15:38:06 +0200

    Added geonames and geolocate classes

---
+++ app/services/geocode.rb
@@ -0,0 +1,40 @@
+require 'net/http'
+require 'json'
+
+class Geocoding
+
+  GOOGLE_MAPS_GEOCODE_API = "AIzaSyBHf-pI5bEXmml-_C4kOFZKSAuGG9eH1kI"
+
+  def initialize(city_name)
+    @city_name = city_name
+  end
+
+  def call
+    geocode = URI("https://maps.googleapis.com/maps/api/geocode/json?address=" + @city_name + "&key=" + GOOGLE_MAPS_GEOCODE_API)
+    geocode_net = Net::HTTP.get_response(geocode)
+    geocode_net_json = JSON.parse(geocode_net.body)
+    longitude = geocode_net_json["results"][0]["geometry"]["location"]["lng"]
+    latitude = geocode_net_json["results"][0]["geometry"]["location"]["lat"]
+
+    country_code = ""
+
+    geocode_net_json["results"][0]["address_components"].each do |short_name|
+      short_name["types"].each do |short_name_types|
+        if short_name_types == "country"
+          country_code = short_name["short_name"]
+        end
+      end
+    end
+
+    location_data = {
+      longitude: longitude,
+       latitude: latitude,
+       country_code: country_code,
+       city_name: @city_name
+     }
+
+    location_data
+
+  end
+end
+
