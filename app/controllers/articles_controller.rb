81485a938a7a273679368312c4116f02d842777d
Giovanni Carrara <carrara.g@gmail.com>
2018-04-25 16:05:46 +0200

    Markers working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
     location_data = Geocode.new(@city_name).call
     @list_of_sorted_cities = Geonames.new(location_data, "app/services/countries.json", "app/services/languages.json").call
 
-    @results = Rails.cache.fetch("articles-#{@city_name}", expires_in: 2.hours) do
+    @results = Rails.cache.fetch("articles-#{@list_of_sorted_cities}", expires_in: 2.hours) do
       Webhose.new(@list_of_sorted_cities).call
     end
   end
