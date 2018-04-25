74e1c764b3c0c06587a10bda892595f35d06e925
Giovanni Carrara <carrara.g@gmail.com>
2018-04-25 15:24:43 +0200

    Integrated caching for webhose API

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,11 @@ class ArticlesController < ApplicationController
   def index
     @city_name = "Milan"
 
-    @location_data = Geocode.new(@city_name).call
-    @list_of_sorted_cities = Geonames.new(@location_data, "app/services/countries.json", "app/services/languages.json").call
-    @results = Webhose.new(@list_of_sorted_cities).call
+    location_data = Geocode.new(@city_name).call
+    @list_of_sorted_cities = Geonames.new(location_data, "app/services/countries.json", "app/services/languages.json").call
+
+    @results = Rails.cache.fetch("articles-#{@city_name}", expires_in: 2.hours) do
+      Webhose.new(@list_of_sorted_cities).call
+    end
   end
 end
