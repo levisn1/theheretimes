fba873be106292c69d515d78e84c2afbe54c2953
Vittorio Molaschi <vittorio.molaschi@gmail.com>
2018-04-26 18:57:37 +0200

    set working date filter

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,21 +1,8 @@
 class Api::V1::ArticlesController < Api::V1::BaseController
   def index
-
-    if params[:lat] && params[:lng]
-      @location_data = ReverseGeocode.new(params).call
-    else
-      @city_name = ""
-      @location_data = Geocode.new(@city_name).call
-    end
-
-    Rails.logger.debug("=======@location_data")
-    Rails.logger.debug(@location_data)
-
-
-    @list_of_sorted_cities = Geonames.new(@location_data, "app/services/countries.json", "app/services/languages.json").call
-
-    @results = Rails.cache.fetch("articles-#{@list_of_sorted_cities}", expires_in: 2.hours) do
-      Webhose.new(@list_of_sorted_cities).call
-    end
+    api = ApplicationRecord.api(params)
+    @city_name = api[:city_name]
+    @list_of_sorted_cities = api[:list_of_sorted_cities]
+    @results = api[:articles]
   end
 end
