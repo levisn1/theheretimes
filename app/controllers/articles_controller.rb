d7c15b2ae0de23df4e19240d50dac2ae81ecff56
Vittorio Molaschi <vittorio.molaschi@gmail.com>
2018-04-30 14:47:45 +0200

    add tooltip in sidenav, real email and bookmark icon in info window

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
   def index
     api = ApplicationRecord.api(params)
     @city_name = api[:city_name]
